aafc7ce49e4b740764d8ad0f57176abf8fc8fbf5
Diego <diego.alvarenga@cloudwalk.io>
2024-08-30 15:13:38 -0300

    feat: add functions to manage transactions in the list

--- expenses/lib/components/transaction_user.dart
+++ expenses/lib/components/transaction_user.dart
@@ -1,17 +1,16 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'transaction_form.dart';
 import 'transaction_list.dart';
 import '../models/transaction.dart';
 
 class TransactionUser extends StatefulWidget {
-  
-
   @override
   State<TransactionUser> createState() => _TransactionUserState();
 }
 
 class _TransactionUserState extends State<TransactionUser> {
-
   final _transactions = [
     Transaction(
       id: 't1',
@@ -23,12 +22,25 @@ class _TransactionUserState extends State<TransactionUser> {
         id: 't2', title: 'Conta de Luz', value: 210.04, date: DateTime.now()),
   ];
 
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         TransactionList(_transactions),
-        TransactionForm(),
+        TransactionForm(_addTransaction),
       ],
     );
   }
