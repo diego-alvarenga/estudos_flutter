80b1b2ed1c1dc4080b9084575ea47a4eb50ad088
Diego <diego.alvarenga@cloudwalk.io>
2024-09-01 20:48:13 -0300

    feat: create chart #05

--- expenses/lib/components/chart.dart
+++ expenses/lib/components/chart.dart
@@ -35,9 +35,14 @@ class Chart extends StatelessWidget {
     });
   }
 
+  double get _weekTotalValue {
+    return groupedTransactions.fold(0.0, (sum, tr) {
+      return sum + (tr['value'] as double);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    groupedTransactions;
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
@@ -46,7 +51,7 @@ class Chart extends StatelessWidget {
           return ChartBar(
             label: tr['day'] as String,
             value: tr['value'] as double,
-            percentage: 0,
+            percentage: (tr['value'] as double) / _weekTotalValue,
           );
         }).toList(),
       ),
