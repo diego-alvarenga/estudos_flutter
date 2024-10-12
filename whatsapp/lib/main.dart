4cc9abad167ee1c5c7909fafbb3cd85e4118467a
Diego Alvarenga Rodrigues <diegoalvarenga@DiegoAlvarengas-MacBook-Air.local>
2024-10-12 13:45:02 -0300

    chore: initial commit and project setup

--- whatsapp/lib/main.dart
+++ whatsapp/lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'Home.dart';
 
-void main() => runApp(MaterialApp(
-  home: Home(),
-  debugShowCheckedModeBanner: false,
-));
+void main() {
+
+  FirebaseFirestore.instance
+  .collection("usuarios")
+  .doc("001")
+  .set({"nome": "Diego"});
+  
+  runApp(MaterialApp(
+    home: const Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
 
