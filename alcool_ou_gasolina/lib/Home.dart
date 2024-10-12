88c8c72dc1f2257c653ce02cd7963de73b9b2adf
Diego Alvarenga Rodrigues <diegoalvarenga@DiegoAlvarengas-MacBook-Air.local>
2024-10-12 14:51:01 -0300

    feat: create class home and add images

---
+++ alcool_ou_gasolina/lib/Home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  
+} 
